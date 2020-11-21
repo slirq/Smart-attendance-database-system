6219ab291b850588852cd46cb3edf0023ac1ee9e
toothsy <c.j.atharva@gmail.com>
2020-11-21 23:15:40 +0530

    camStuff2

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const signIn = require('./controllers/signIn');
+const cam = require('./controllers/cam');
 const cors = require('cors');
 const port = 5000;
 const app = express();
 app.use(express.json({ extended: false }) );
 app.use(cors())
 app.post('/signIn',signIn.signIn)
+app.get('/cam',cam.cam)
 app.listen(port,()=>{console.log(`server is running on ${port}`)})  
