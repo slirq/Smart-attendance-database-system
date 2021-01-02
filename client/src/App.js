a9537b7c01cb76d676a6bd185aab65ec8ff91884
abdullah <abdullahshaikh136@gmail.com>
2021-01-02 15:43:21 +0530

    Revert "done with student"
    This reverts commit 1b016b15981ea8edf7d5e1ae98f2399264235439.

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import './App.css';
 import React from 'react'
 import SignIn from './components/signIn'
 import {UserContext} from './context/context'
@@ -16,8 +17,8 @@ function App() {
           </Route>
           <Route exact path='/signIn' component={SignIn} />
           <Route exact path='/signUp' component={SignUp} />
-          <Route exact path='/staffDashBoard' component={StaffDashBoard} />
-          <Route exact path='/stuDashBoard' component={StuDashBoard} />
+          <ProtectedRoute exact path='/staffDashBoard' component={StaffDashBoard} />
+          <ProtectedRoute exact path='/stuDashBoard' component={StuDashBoard} />
         </Switch>
       </UserContext>
     </Router>
